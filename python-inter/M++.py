a0cac2be05d34ba1a69f99178d62b24167f9d9f5
PeerSendX <jupiterxxx11@gmail.com>
2022-03-01 00:16:35 +0000

    interlang with basic commands

---
+++ python-inter/M++.py
@@ -0,0 +1,51 @@
+import Mpp
+import time
+import os
+###################
+# Help instructions
+
+help = """
+RUN("file.mpp")
+PRINT("string") // Dont try to print number
+FUN ==>> Is the same us function()
+"""
+
+
+
+
+
+
+###################
+
+print("M++ Interpreter")
+
+
+
+while True:
+	
+	text = input('M++ > ')
+	history = []
+	if text == "HELP":
+		print(help)
+		text = "0.0" # help 0.0
+	elif text == "history":
+		print(history)
+		text = "0.0" # History == 0.00
+	elif text == "exit":
+		print(":)")
+		time.sleep(1)
+		print(":(")
+		time.sleep(1)
+		print("bye")
+		exit()
+	if text.strip() == "": continue
+	result, error = Mpp.run('<stdin>', text)
+	history.append(text)
+
+	if error:
+		print(error.as_string())
+	elif result:
+		if len(result.elements) == 1:
+			print(repr(result.elements[0]))
+		else:
+			print(repr(result))
